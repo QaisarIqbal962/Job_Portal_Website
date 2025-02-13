a3cf0d8ab4d535a713b3c282956da0f7ecbe0423
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-02-13 11:06:05 +0500

    done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const sortBtns = document.querySelectorAll(".job-id > *");
-const sortItems = document.querySelectorAll(".jobs-container > *"); // Fixed selector
+const sortItems = document.querySelectorAll(".jobs-container > *"); 
 
 sortBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     sortBtns.forEach((btn) => btn.classList.remove("active"));
     btn.classList.add("active");
 
-    const targetData = btn.getAttribute("data-target").trim(); // Trim whitespace
+    const targetData = btn.getAttribute("data-target").trim(); 
     sortItems.forEach((item) => {
       item.classList.add("delete");
       if (item.getAttribute("data-item") === targetData || targetData === "all") {
