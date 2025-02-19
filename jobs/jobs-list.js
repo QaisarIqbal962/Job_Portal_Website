134d4d621f28fa94ca542a72c3c7a491c3444136
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-02-19 22:10:08 +0500

    add more jobs in job listing file

--- jobs/jobs-list.js
+++ jobs/jobs-list.js
@@ -31,4 +31,38 @@ const jCategory = [
         education: "Degree in any Computer Science Course.",
         experience: "1 to 5 year(s)",
     },
+
+
+    {
+        index: 2,
+        image: "../images/Linkedin.png",
+        title: "Business Associate",
+        rate: "$900-1200/m",
+        av: "Part Time",
+        companyName: "Business Associate",
+        location: "USA",
+        vacancy: "04",
+        hours: "40h / week",
+        description: "it is a long established fact that a reader will be distracted by the readable content of page when looking at its layout.The point of using lorem ipsum is that it hsa a more-or less normal distribution of letters, as opposed to using 'Content here,content here'making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.Various versions have evolved over the years, sometimes by accident, sometime on purpose (injected humour and the like).",
+        workplace: "Work from office",
+        education: "Degree in any Computer Science Course.",
+        experience: "1 to 5 year(s)",
+    },
+
+    {
+    index: 3,
+    image: "../images/facebook.png",
+    title: "Digital Marketing",
+    rate: "$900-1200/m",
+    av: "Full Time",
+    companyName: "Digital Marketing",
+    location: "USA",
+    vacancy: "05",
+    hours: "45h / week",
+    description: "it is a long established fact that a reader will be distracted by the readable content of page when looking at its layout.The point of using lorem ipsum is that it hsa a more-or less normal distribution of letters, as opposed to using 'Content here,content here'making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.Various versions have evolved over the years, sometimes by accident, sometime on purpose (injected humour and the like).",
+    workplace: "Work from Home",
+    education: "Degree in any Computer Science Course.",
+    experience: "1 to 4 year(s)",
+    },
+    
 ];
