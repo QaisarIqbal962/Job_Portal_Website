a85ba945edf9059c6b60f979a455256ee4fbc29a
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-02-19 22:27:32 +0500

    updated

--- jobs/jobs-list.js
+++ jobs/jobs-list.js
@@ -113,5 +113,21 @@ const jCategory = [
                 education: "Degree in any Computer Science Course.",
                 experience: "1 to 4 year(s)",
                 },
+
+                {
+                    index: 4,
+                    image: "../images/freelancer.png",
+                    title: "Freelancer",
+                    rate: "$900-1200/m",
+                    av: "Part Time",
+                    companyName: "Freelancer",
+                    location: "USA",
+                    vacancy: "05",
+                    hours: "25h / week",
+                    description: "it is a long established fact that a reader will be distracted by the readable content of page when looking at its layout.The point of using lorem ipsum is that it hsa a more-or less normal distribution of letters, as opposed to using 'Content here,content here'making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.Various versions have evolved over the years, sometimes by accident, sometime on purpose (injected humour and the like).",
+                    workplace: "Work from Home",
+                    education: "Degree in any Computer Science Course.",
+                    experience: "1 to 2 year(s)",
+                    },
     
 ];
